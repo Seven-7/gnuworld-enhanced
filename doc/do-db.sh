28413a6966b09f2215a02e72f6613fe40869e5fa
Gergo F <gergo_f@yahoo.com>
2016-03-11 19:21:11 +0000

    Added simple database-creation script

---
+++ doc/do-db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+/usr/local/pgsql/bin/createdb --template=template0 -E=SQL_ASCII -U gnuworld cservice
+/usr/local/pgsql/bin/psql -U gnuworld cservice < cservice.sql
+/usr/local/pgsql/bin/psql cservice < cservice.config.sql
+/usr/local/pgsql/bin/psql cservice < languages.sql
+/usr/local/pgsql/bin/psql cservice < language_table.sql
+/usr/local/pgsql/bin/psql cservice < greeting.sql
+/usr/local/pgsql/bin/psql cservice < cservice.help.sql
+/usr/local/pgsql/bin/psql cservice < cservice.web.sql
+/usr/local/pgsql/bin/psql cservice < cservice.addme.sql
+/usr/local/pgsql/bin/createdb --template=template0 -E=SQL_ASCII ccontrol
+/usr/local/pgsql/bin/psql ccontrol < ccontrol.sql
+/usr/local/pgsql/bin/psql ccontrol < ccontrol.help.sql
+/usr/local/pgsql/bin/psql ccontrol < ccontrol.addme.sql
+/usr/local/pgsql/bin/psql ccontrol < ccontrol.commands.sql
+/usr/local/pgsql/bin/createdb --template=template0 -E=SQL_ASCII dronescan
+/usr/local/pgsql/bin/psql dronescan < dronescan.sql
+
